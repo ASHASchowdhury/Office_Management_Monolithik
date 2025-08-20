a0e96a404cd10ed13de11d570f4286f7c40a4d19
Abrar Labib <labibchowdhury1234@gmail.com>
2025-08-20 17:57:04 +0600

    adding all

---
+++ src/main/java/com/OfficeManagement/OfficeProject/OfficeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.OfficeManagement.OfficeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OfficeManagement/OfficeProject/OfficeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OfficeManagement.OfficeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
