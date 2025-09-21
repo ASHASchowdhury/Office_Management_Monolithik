aa0def5f4ac126972adb19edb387dc59bbf6cd59
Abrar <labibchowdhury1234@gmail.com>
2025-09-22 03:06:04 +0600

    the last update 22 sep

--- src/main/java/com/OfficeManagement/OfficeProject/dtos/DepartmentDTO.java
+++ src/main/java/com/OfficeManagement/OfficeProject/dtos/DepartmentDTO.java
@@ -6,29 +6,22 @@ public class DepartmentDTO {
     private Long id;
     private String name;
 
+    public DepartmentDTO() {}
 
-
-    public DepartmentDTO(Long deptId, String deptName){};
+    public DepartmentDTO(Long deptId, String deptName) {
+        this.id = deptId;
+        this.name = deptName;
+    }
 
     public DepartmentDTO(Long id, String name, List<String> employeeNames) {
         this.id = id;
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
+    // Getters & Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 }
